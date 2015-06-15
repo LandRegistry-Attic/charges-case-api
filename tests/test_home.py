7410caf73ddac32445d35caff43629c45545f4f2
endamccormack <e.mccormack@kainos.com>
2015-06-15 11:51:40 +0100

    Added tests for helpers, cases and the new home url. Database reliant methods are untested as they create a dependency on another component

---
+++ tests/test_home.py
@@ -0,0 +1,15 @@
+from tests.helpers import with_client, setUpApp, with_context
+import unittest
+
+
+class TestHome (unittest.TestCase):
+
+    def setUp(self):
+        setUpApp(self)
+
+    @with_context
+    @with_client
+    def test_get_api(self, client):
+        response = client.get('/')
+        assert response.status_code == 200
+        assert '"Hello": "World"' in response.data.decode()
