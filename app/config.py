6d7d38d4fc919309107cdf94adeb22ec15ec3377
endamccormack <endamccormack0@gmail.com>
2015-06-29 10:57:51 +0100

    Using default for config rather than error checking when the variable isnt there

--- app/config.py
+++ app/config.py
@@ -2,8 +2,4 @@ import os
 
 DEBUG = True
 
-try:
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URI']
-except KeyError:
-    print("[ERROR] You need set the export variable for DATABASE_URI")
-    raise
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', '')
