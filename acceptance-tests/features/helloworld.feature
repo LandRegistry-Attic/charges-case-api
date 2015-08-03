93d80b04ee7db64e5c7aa8d821af0dfeb580aad7
nigelpain <nigel.pain@digital.landregistry.gov.uk>
2015-08-03 15:49:07 +0100

    removed helloworld tests now actual acc tests are present

--- acceptance-tests/features/helloworld.feature
+++
@@ -1,5 +0,0 @@
-Feature: Check helloworld API
-
-Scenario: Check helloworld API
-    Given I access the helloworld API
-    Then the response contains Hello World
--- acceptance-tests/features/steps/step_definitions.rb
+++
@@ -1,8 +0,0 @@
-Given(/^I access the helloworld API$/) do
-  response = Net::HTTP.get_response(URI($CASE_API_URL + '/helloworld'))
-  $apiData = MultiJson.load(response.body)
-end
-
-Then(/^the response contains Hello World$/) do
-  assert_match('World', $apiData['Hello'], 'Couldnt find text World')
-end
