ceca8c3aaff03ee5b10dc122bc30febfa4a8f93f
nigelpain <nigel.pain@digital.landregistry.gov.uk>
2015-08-19 14:12:00 +0100

    added retrieve case function using httprb and amended existing steps to work with it

--- acceptance-tests/features/steps/general_definitions.rb
+++ acceptance-tests/features/steps/general_definitions.rb
@@ -1,3 +1,7 @@
 Given(/^I have created the following case:$/) do |case_json|
-  @case_id = create_case_data(case_json)
+  @created_case = create_case_data(case_json)
+end
+
+When(/^I retrieve the created case$/) do
+  @retrieved_case = get_case_data(@created_case['id'])
 end
--- acceptance-tests/features/support/functions.rb
+++ acceptance-tests/features/support/functions.rb
@@ -2,9 +2,19 @@ def create_case_data(case_json)
   case_json = JSON.parse(case_json)
   response = HTTP.post($CASE_API_URL + '/case', json: case_json)
   if response.code == 201
-    JSON.parse(response.body)['id']
+    JSON.parse(response.body)
   else
     fail "Error: Couldn't create case #{case_json}, "\
+            "received response #{response.code}"
+  end
+end
+
+def get_case_data(case_id)
+  response = HTTP.get($CASE_API_URL + '/case/' + case_id.to_s)
+  if response.code == 200
+    JSON.parse(response.body)
+  else
+    fail "Error: Couldn't retrieve case #{case_id}, "\
             "received response #{response.code}"
   end
 end
