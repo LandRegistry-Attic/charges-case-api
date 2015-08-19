19aea684b39a80a40ce5baa3f35de37a14d0c171
Dawid Stelmaszek <d.stelmaszek@kainos.com>
2015-08-19 13:51:54 +0100

    add borrowers endpoint/getting cases with borrowers

--- app/borrower/server.py
+++ app/borrower/server.py
@@ -1,8 +1,23 @@
+from flask import request, abort
+from flask.ext.api import status
+from app.borrower.model import Borrower
 
 
 def register_routes(blueprint):
-    @blueprint.route('/case/<case_id>/borrowers', methods=['GET'])
-    def get_borrowers(case_id):
-        stuff = [borrwers.to_json() for case in Case.all()]
+    @blueprint.route('/case/<case_id>/borrowers', methods=['POST'])
+    def add_borrowers(case_id):
+        request_data = request.data['borrowers']
 
-        return stuff
+        def add_case_id(borrower):
+            borrower['case_id'] = case_id
+            return borrower
+
+        borrowers = [add_case_id(item) for item in request_data]
+
+        try:
+            Borrower.add(borrowers)
+        except Exception as exc:
+            print(str(type(exc)) + ":" + str(exc))
+            abort(status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        return {'status_code': status.HTTP_200_OK}
