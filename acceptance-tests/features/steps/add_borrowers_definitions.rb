2288b026ed48f3daf84fcd24525010983f96b89b
nigelpain <nigel.pain@digital.landregistry.gov.uk>
2015-08-19 16:27:29 +0100

    added step for add borrower with missing mandatory info

---
+++ acceptance-tests/features/steps/add_borrowers_definitions.rb
@@ -0,0 +1,20 @@
+When(/^I try to add a borrower with missing mandatory information$/) do
+  borrower_json = {
+    "borrowers"=>[
+      {
+        "first_name"=>"Paul",
+        "last_name"=>"Smith",
+        "email_address"=>"psmith@yahoo.co.uk",
+        "type"=>"Borrower",
+        "mobile_no"=>"",
+        "middle_names"=>"",
+        "address"=>[
+          "83 Lordship Park",
+          "London",
+          "N16 5UT"
+        ]
+      }
+    ]
+  }
+  response = HTTP.post($CASE_API_URL + '/case/' + @created_case['id'].to_s + '/borrowers', json: borrower_json)
+end
