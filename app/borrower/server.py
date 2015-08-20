94f16144f60c4ea32cfe02f77740f7a6be50b7a4
Dawid Stelmaszek <d.stelmaszek@kainos.com>
2015-08-20 11:53:07 +0100

    get borrowers endpoint; unit tests

--- app/borrower/server.py
+++ app/borrower/server.py
@@ -6,7 +6,20 @@ from app.borrower.model import Borrower
 def register_routes(blueprint):
     @blueprint.route('/case/<case_id>/borrowers', methods=['POST'])
     def add_borrowers(case_id):
+        def validate_request_data():
+            def in_and_not_empty(val, borrower):
+                return val in borrower and borrower[val]
+
+            for borrower_ in request_data:
+                if not (in_and_not_empty('first_name', borrower_) and
+                        in_and_not_empty('last_name', borrower_) and
+                        in_and_not_empty('mobile_no', borrower_) and
+                        in_and_not_empty('email_address', borrower_) and
+                        in_and_not_empty('address', borrower_)):
+                    abort(status.HTTP_400_BAD_REQUEST)
+
         request_data = request.data['borrowers']
+        validate_request_data()
 
         def add_case_id(borrower):
             borrower['case_id'] = case_id
@@ -21,3 +34,12 @@ def register_routes(blueprint):
             abort(status.HTTP_500_INTERNAL_SERVER_ERROR)
 
         return {'status_code': status.HTTP_200_OK}
+
+    @blueprint.route('/case/<case_id>/borrowers', methods=['GET'])
+    def get_borrowers(case_id):
+        borrowers = Borrower.get_by_case_id(case_id)
+
+        if borrowers is None:
+            abort(status.HTTP_404_NOT_FOUND)
+
+        return [borrower.to_json() for borrower in borrowers]
