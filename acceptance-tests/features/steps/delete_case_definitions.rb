6a65c50ca5970d17d6614a4c979fb3446ba05e53
nigelpain <nigel.pain@digital.landregistry.gov.uk>
2015-09-03 15:52:36 +0100

    added steps to try retrieve borrower and property when case deleted

--- acceptance-tests/features/steps/delete_case_definitions.rb
+++ acceptance-tests/features/steps/delete_case_definitions.rb
@@ -1,3 +1,17 @@
 Given(/^I delete the created case$/) do
   delete_case_data(@created_case['id'])
 end
+
+When(/^I attempt to retrieve the deleted case$/) do
+  @response = HTTP.get(Env.domain + '/case/' + @created_case['id'].to_s)
+end
+
+When(/^I attempt to retrieve the borrower$/) do
+  @response = HTTP.get(Env.domain + '/case/' + @created_case['id'].to_s +
+                      '/borrowers')
+end
+
+When(/^I attempt to retrieve the property$/) do
+  @response = HTTP.get(Env.domain + '/case/' + @created_case['id'].to_s +
+                      '/property')
+end
--- acceptance-tests/features/support/case_functions.rb
+++ acceptance-tests/features/support/case_functions.rb
@@ -55,7 +55,7 @@ end
 def get_property_for_case(case_id)
   response = HTTP.get(Env.domain + '/case/' + case_id.to_s + '/property')
   if response.code == 200
-    puts "Case #{case_id} has been deleted."
+    JSON.parse(response.body)
   else
     fail "Error: Couldn't retrieve property for case #{case_id}, "\
             "received response #{response.code}"
