7341b9a493357ec2c75604d4d51346b46160db88
nigelpain <nigel.pain@digital.landregistry.gov.uk>
2015-09-30 09:40:25 +0100

    reworked test data clear up to work with refactor data helpers

--- acceptance-tests/features/support/hooks.rb
+++ acceptance-tests/features/support/hooks.rb
@@ -11,8 +11,12 @@ end
 
 ### Code that should be executed once all of the acceptance tests have run.
 After ('@delete_test_data') do
-  if @created_case
-    puts "Deleting test case #{@created_case['id']}..."
-    delete_case_data(@created_case['id'])
+  if @created_deed_id
+    puts "Deleting test deed #{@created_deed_id}"
+    delete_deed_data(@created_deed_id)
+  end
+  if @created_case_id
+    puts "Deleting test case #{@created_case_id}"
+    delete_case_data(@created_case_id)
   end
 end
