f558f777e9f22af984f88ec784f2709cc3fceccf
marknorman <mark@kirosoft.co.uk>
2015-10-13 09:59:08 +0100

    Initial change for submit

---
+++ app/service/deed_api.py
@@ -0,0 +1,14 @@
+import requests
+from app import config
+
+
+class DeedApi(object):
+    DEED_API_BASE_HOST = config.DEED_API_BASE_HOST
+
+    def get(self, deed_id):
+        url = "{base}/deed/{deed_id}".format(
+            base=self.DEED_API_BASE_HOST,
+            deed_id=str(deed_id)
+        )
+        return requests.get(url)
+
