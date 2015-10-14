f54ab8aea31b8b376ab4feb0ff17cf3080d46e2b
Dan Marker <daniel.marker@digital.landregistry.gov.uk>
2015-10-14 16:44:28 +0100

    Added config import - tests now working

--- app/config.py
+++ app/config.py
@@ -3,5 +3,6 @@ import os
 DEBUG = True
 
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'postgres:///case_api')
+
 DEED_API_BASE_HOST = os.getenv('DEED_API_BASE_HOST',
                                'http://deedapi.dev.service.gov.uk')
