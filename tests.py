4fb4351c1ebdbfd26f19f67cc4e2b5b3592a4e14
marknorman <mark@kirosoft.co.uk>
2015-10-19 16:17:00 +0100

    Add optional debug argument to tests.py to allow remote debugging e.g
    > python3 tests.py debug

--- tests.py
+++ tests.py
@@ -5,9 +5,11 @@ from xmlrunner import XMLTestRunner
 from colour_runner.runner import ColourTextTestRunner
 from os import path
 
-# sys.path.append("./bin/pycharm-debug-py3k.egg")
-# import pydevd
-# pydevd.settrace('10.0.2.2', port=8085, stdoutToServer=True, stderrToServer=True)
+
+if (len(sys.argv) > 1 and sys.argv[1].lower() == "debug"):
+    sys.path.append("./bin/pycharm-debug-py3k.egg")
+    import pydevd
+    pydevd.settrace('10.0.2.2', port=8085, stdoutToServer=True, stderrToServer=True)
 
 if (len(sys.argv) > 1 and sys.argv[1].lower() == "debug"):
     sys.path.append("./bin/pycharm-debug-py3k.egg")
