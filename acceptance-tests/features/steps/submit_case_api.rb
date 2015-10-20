153b63563d12d50b37e9b9751d174e9623d25535
cs112mm <mandy.nimmo@digital.landregistry.gov.uk>
2015-10-20 08:36:34 +0100

    Acceptance tests for US90

--- acceptance-tests/features/steps/submit_case_api.rb
+++ acceptance-tests/features/steps/submit_case_api.rb
@@ -6,3 +6,8 @@ When(/^I submit the case again$/) do
   @response = HTTP.post(Env.case_api + '/case/' + @created_case_id.to_s +
   '/application')
 end
+
+When(/^I submit the case via the API$/) do
+  @response = HTTP.post(Env.case_api + '/case/' + @created_case_id.to_s +
+  '/application')
+end
