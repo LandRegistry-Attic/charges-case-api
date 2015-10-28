ba7b39e2172b76e8ab29b001a3299349da686438
User1 <user1@LRAdmins-MacBook-Pro.local>
2015-10-28 15:56:50 +0000

    Create case acceptance test with more validators

--- acceptance-tests/features/steps/create_case.rb
+++ acceptance-tests/features/steps/create_case.rb
@@ -8,4 +8,8 @@ And(/^the right details are returned$/) do
   assert(JSON.parse(@response.body)['id'],
          'Error: id not returned ' + @response.status_code.to_s)
   assert(JSON.parse(@response.body)['id'].is_a? Integer)
+  assert(DateTime.parse(JSON.parse(@response.body)['created_on']).is_a? Date)
+  assert(DateTime.parse(JSON.parse(@response.body)['last_updated']).is_a? Date)
+  assert(JSON.parse(@response.body)['type'] == 'Case')
+  assert(JSON.parse(@response.body)['status'] == 'Case created')
 end
